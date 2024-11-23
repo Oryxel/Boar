8dfdb216d467cadc1b7a13c2a5165d2d65428a46
Oryxel <172038521+Oryxel@users.noreply.github.com>
2024-11-23 16:56:14 +0700

    preparing for prediction, effect.

--- src/main/java/ac/boar/anticheat/packets/EffectUpdatePacket.java
+++ src/main/java/ac/boar/anticheat/packets/EffectUpdatePacket.java
@@ -1,9 +1,11 @@
 package ac.boar.anticheat.packets;
 
+import ac.boar.anticheat.data.StatusEffect;
 import ac.boar.anticheat.user.api.BoarPlayer;
 import ac.boar.protocol.event.java.PacketListener;
 import ac.boar.protocol.event.java.PacketSendEvent;
 import org.geysermc.geyser.entity.type.Entity;
+import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.entity.ClientboundRemoveMobEffectPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.entity.ClientboundUpdateMobEffectPacket;
 
 public class EffectUpdatePacket implements PacketListener {
@@ -22,7 +24,27 @@ public class EffectUpdatePacket implements PacketListener {
                 return;
             }
 
+            player.sendTransaction();
+            player.latencyUtil.addTransactionToQueue(player.lastSentId, () -> {
+                player.statusMap.put(packet.getEffect(), new StatusEffect(packet.getEffect(), packet.getAmplifier(), packet.getDuration()));
+            });
+        }
 
+        if (event.packet() instanceof ClientboundRemoveMobEffectPacket) {
+            final ClientboundRemoveMobEffectPacket packet = (ClientboundRemoveMobEffectPacket) event.packet();
+            Entity entity = player.getSession().getEntityCache().getEntityByJavaId(packet.getEntityId());
+            if (entity == null) {
+                return;
+            }
+
+            if (entity != player.getSession().getPlayerEntity()) {
+                return;
+            }
+
+            player.sendTransaction();
+            player.latencyUtil.addTransactionToQueue(player.lastSentId, () -> {
+                player.statusMap.remove(packet.getEffect());
+            });
         }
     }
 }
