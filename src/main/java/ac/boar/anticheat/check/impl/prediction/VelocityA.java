2903ff4efcb5bfeed8f10c31d783dbaf72b3b5c9
Oryxel <172038521+Oryxel@users.noreply.github.com>
2024-11-30 17:06:44 +0700

    Proper velocity check?

--- src/main/java/ac/boar/anticheat/check/impl/prediction/VelocityA.java
+++ src/main/java/ac/boar/anticheat/check/impl/prediction/VelocityA.java
@@ -5,6 +5,7 @@ import ac.boar.anticheat.check.api.impl.OffsetHandlerCheck;
 import ac.boar.anticheat.prediction.engine.data.VectorType;
 import ac.boar.anticheat.user.api.BoarPlayer;
 import ac.boar.utils.math.Vec3d;
+import org.bukkit.Bukkit;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -23,14 +24,14 @@ public class VelocityA extends OffsetHandlerCheck {
 
         Iterator<Map.Entry<Long, Vec3d>> iterator = player.queuedVelocities.entrySet().iterator();
 
-//        Map.Entry<Long, Vec3d> entry;
-//        while (iterator.hasNext() && (entry = iterator.next()) != null) {
-//            if (entry.getKey() < player.lastReceivedId) {
-//                break;
-//            }
-//
-//            Bukkit.broadcastMessage("fail VelocityA!");
-//            iterator.remove();
-//        }
+        Map.Entry<Long, Vec3d> entry;
+        while (iterator.hasNext() && (entry = iterator.next()) != null) {
+            if (player.lastReceivedId - entry.getKey() < 2) {
+                break;
+            }
+
+            Bukkit.broadcastMessage("fail VelocityA!");
+            iterator.remove();
+        }
     }
 }
