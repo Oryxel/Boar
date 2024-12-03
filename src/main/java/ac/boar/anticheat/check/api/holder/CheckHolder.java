5a5895448f3adaa3fd92f820e482ff312274b6fe
Oryxel <172038521+Oryxel@users.noreply.github.com>
2024-12-03 21:53:10 +0700

    delete the reach check since it doesn't works anyway.

--- src/main/java/ac/boar/anticheat/check/api/holder/CheckHolder.java
+++ src/main/java/ac/boar/anticheat/check/api/holder/CheckHolder.java
@@ -1,7 +1,6 @@
 package ac.boar.anticheat.check.api.holder;
 
 import ac.boar.anticheat.check.api.Check;
-import ac.boar.anticheat.check.impl.combat.ReachA;
 import ac.boar.anticheat.check.impl.prediction.PredictionA;
 import ac.boar.anticheat.check.impl.velocity.VelocityA;
 import ac.boar.anticheat.check.impl.timer.TimerA;
@@ -18,7 +17,6 @@ public class CheckHolder extends HashMap<Class, Check> {
         this.put(PredictionA.class, new PredictionA(player));
         this.put(VelocityA.class, new VelocityA(player));
 
-        this.put(ReachA.class, new ReachA(player));
         this.put(TimerA.class, new TimerA(player));
     }
 }
