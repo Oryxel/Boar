44b92955040844df30fda53ff6d85112c8ddc6fa
Oryxel <172038521+Oryxel@users.noreply.github.com>
2024-12-03 18:18:13 +0700

    semi movement attribute support.

--- src/main/java/ac/boar/anticheat/packets/AbilitiesUpdatePacket.java
+++ src/main/java/ac/boar/anticheat/packets/AbilitiesUpdatePacket.java
@@ -4,31 +4,50 @@ import ac.boar.anticheat.user.api.BoarPlayer;
 import ac.boar.protocol.event.bedrock.geyser.GeyserPacketListener;
 import ac.boar.protocol.event.bedrock.geyser.GeyserSendEvent;
 import org.cloudburstmc.protocol.bedrock.data.AbilityLayer;
+import org.cloudburstmc.protocol.bedrock.data.AttributeData;
 import org.cloudburstmc.protocol.bedrock.packet.UpdateAbilitiesPacket;
+import org.cloudburstmc.protocol.bedrock.packet.UpdateAttributesPacket;
+import org.geysermc.geyser.entity.attribute.GeyserAttributeType;
 
 public class AbilitiesUpdatePacket implements GeyserPacketListener {
     @Override
     public void onPacketSend(GeyserSendEvent event) {
-        if (!(event.getPacket() instanceof UpdateAbilitiesPacket)) {
-            return;
-        }
         final BoarPlayer player = event.getPlayer();
+        if (event.getPacket() instanceof UpdateAbilitiesPacket) {
+            final UpdateAbilitiesPacket updateAbilitiesPacket = (UpdateAbilitiesPacket) event.getPacket();
+            if (updateAbilitiesPacket.getUniqueEntityId() != player.getSession().getPlayerEntity().getGeyserId()) {
+                return;
+            }
 
-        final UpdateAbilitiesPacket updateAbilitiesPacket = (UpdateAbilitiesPacket) event.getPacket();
-        if (updateAbilitiesPacket.getUniqueEntityId() != player.getSession().getPlayerEntity().getGeyserId()) {
-            return;
+            event.getPostTasks().add(() -> player.sendTransaction(true));
+            player.latencyUtil.addTransactionToQueue(player.lastSentId + 1, () -> {
+                player.abilities.getAbilities().clear();
+                for (AbilityLayer layer : updateAbilitiesPacket.getAbilityLayers()) {
+                    if (layer.getLayerType() == AbilityLayer.Type.BASE) {
+                        player.abilities.setWalkSpeed(layer.getWalkSpeed());
+                    }
+
+                    player.abilities.getAbilities().addAll(layer.getAbilityValues());
+                }
+            });
         }
 
-        event.getPostTasks().add(() -> player.sendTransaction(true));
-        player.latencyUtil.addTransactionToQueue(player.lastSentId + 1, () -> {
-            player.abilities.getAbilities().clear();
-            for (AbilityLayer layer : updateAbilitiesPacket.getAbilityLayers()) {
-                if (layer.getLayerType() == AbilityLayer.Type.BASE) {
-                    player.abilities.setWalkSpeed(layer.getWalkSpeed());
+        if (event.getPacket() instanceof UpdateAttributesPacket) {
+            final UpdateAttributesPacket updateAttributesPacket = (UpdateAttributesPacket) event.getPacket();
+            if (updateAttributesPacket.getRuntimeEntityId() != player.getSession().getPlayerEntity().getGeyserId()) {
+                return;
+            }
+
+            for (AttributeData attributeData : updateAttributesPacket.getAttributes()) {
+                if (!attributeData.getName().equalsIgnoreCase(GeyserAttributeType.MOVEMENT_SPEED.getBedrockIdentifier())) {
+                    continue;
                 }
 
-                player.abilities.getAbilities().addAll(layer.getAbilityValues());
+                player.latencyUtil.addTransactionToQueue(player.lastSentId + 1, () -> {
+                    player.abilities.setWalkSpeed(attributeData.getValue());
+                });
+                event.getPostTasks().add(() -> player.sendTransaction(true));
             }
-        });
+        }
     }
 }
