1b7a437d941af79e6ca0e7620c2d55a302532523
Oryxel <172038521+Oryxel@users.noreply.github.com>
2024-12-12 18:07:31 +0700

    only allow for 0.006 offset.

--- src/main/java/ac/boar/anticheat/prediction/StupidWorkAround.java
+++ src/main/java/ac/boar/anticheat/prediction/StupidWorkAround.java
@@ -14,7 +14,7 @@ public class StupidWorkAround {
             float lengthPrediction = after.length();
             float lengthActual = player.actualVelocity.length();
 
-            if (lengthPrediction > lengthActual && lengthPrediction - lengthActual < 0.05) {
+            if (lengthPrediction > lengthActual && after.squaredDistanceTo(player.actualVelocity) <= 0.000036) {
                 fixed = player.actualVelocity.clone();
             }
         }
